45be1d5a9544af70bb53450b47a6eb708e5d1784
Miguel <michshk@gmail.com>
2017-08-05 10:35:02 -0500

    added use item

---
+++ src/useItem.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// functions for using each item
+
+module.exports.useRock = function(message, mentionedUser){
+    //use the rock at the user
+
+
+    // update inventory
+
+    // update tacos for mentioned user
+}
+
+module.exports.usePieceOfWood = function(message, mentionedUser){
+    // update inventory
+
+    // update tacos for mentioned user
+}
