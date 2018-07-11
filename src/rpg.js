8b10833992985d1ee5aeddd46934d403bd64d1b1
Miguel <michshk@gmail.com>
2018-07-11 17:59:36 -0500

    fixed armor over 75%

--- src/rpg.js
+++ src/rpg.js
@@ -3593,9 +3593,9 @@ function calculateDamageDealt(event, caster, target, rpgAbility){
 
                 if (damageToReduce > 0.75){
                     damageToReduce = 0.75
-                }else{
-                    baseDamage = baseDamage * (1 - damageToReduce);
                 }
+                baseDamage = baseDamage * (1 - damageToReduce);
+                
                 // additional RNG
                 var rngDmgRoll = Math.floor(Math.random() * Math.floor(baseDamage * 0.07)) + 1;
                 baseDamage = baseDamage  + rngDmgRoll;
