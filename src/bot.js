0751a9f84f0439c0588799ee6f091f81b63261df
Miguel <michshk@gmail.com>
2018-10-17 12:41:07 -0500

    added seasonal to other servers

--- src/bot.js
+++ src/bot.js
@@ -611,11 +611,11 @@ client.on('message', function(message){
                         message.channel.send(error);
                     }
                 }
-                /* SEASONAL
+                
                 else if (commandIs("trickortreat", message)){
                     commands.trickOrTreatCommand(message);
                 }
-                
+                /* SEASONAL
                 else if (commandIs("present", message)){
                     commands.openPresentCommand(message);
                 }
