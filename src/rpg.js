e96e5f05c48d091a776e6fcfab4a860ec370f018
Miguel <michshk@gmail.com>
2018-12-23 15:09:15 -0600

    added rolls to 11

--- src/rpg.js
+++ src/rpg.js
@@ -1922,7 +1922,7 @@ function calculateRewards(event, memberInRpgEvent, getItemResponse, numberOfMemb
         if (event.challenge.challenge == 9){
             numberOfRolls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
         }
-        if (event.challenge.challenge == 10){
+        if (event.challenge.challenge >= 10){
             numberOfRolls = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                             22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
         }
