59407c1149476ee7816f3b549d4311beb786857a
Miguel <michshk@gmail.com>
2019-01-05 19:22:16 -0600

    added fix for bidding

--- src/commands.js
+++ src/commands.js
@@ -7579,7 +7579,7 @@ module.exports.marketBidCommand = function(message, args){
                 agreeToTerms(message, discordUserId);
             }else{
                 var userTacosToBid = profileRes.data.tacos;
-                if (userTacosToBid - tacosInUseAuction[discordUserId] >= biddingTacos){
+                if (userTacosToBid - ( tacosInUseAuction[discordUserId] || 0 ) >= biddingTacos){
                     var auctionToBidOn = marketItems[idOfItemToBid] ? marketItems[idOfItemToBid] : 0;
                     // your bid has to be higher than current bid + 5% of current bid
                     var adjustedCurrentBid = Math.floor( auctionToBidOn.currentbid + (auctionToBidOn.currentbid * .03) )
